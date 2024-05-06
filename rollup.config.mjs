c20620633a460815c154189a88af6da0386043e4
Shubhanshu Bahuguna <Shubhanshu.Bahuguna@MacBook-Pro.local>
2024-05-06 15:29:09 +0530

    commit

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,6 +4,7 @@ import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
 import packageJson from "./package.json" assert { type: "json" };
+import terser from '@rollup/plugin-terser';
 
 export default [
   {
@@ -25,6 +26,7 @@ export default [
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       postcss(),
+      terser()
     ],
   },
   {
